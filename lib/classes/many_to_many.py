c40e876be07252ef106a5099362a25709fb11556
David <wjw3298@gmail.com>
2023-09-20 16:20:02 -0400

    complete challenge v2

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -2,22 +2,23 @@ class Game:
     def __init__(self, title):
         self.title = title
         self.results_list = []
-        self.players_list = []
+        self.player_list = []
 
     @property
     def title(self):
         return self._title
     
     @title.setter
-    def title(self, title_parameter):
-        if (not hasattr(self, 'title')) and (type(title_parameter) == str) and (len(title_parameter) > 0):
-            self._title = title_parameter
+    def title(self, title):
+        if (not (hasattr(self, 'title'))) and isinstance(title, str) and len(title) > 0:
+            self._title = title
+
 
     def results(self):
         return self.results_list
 
     def players(self):
-        return self.players_list
+        return self.player_list
 
     def average_score(self, player):
         score_list = [result.score for result in self.results_list if result.player is player]
@@ -26,57 +27,54 @@ class Game:
 class Player:
     def __init__(self, username):
         self.username = username
-        self.results_list =[]
-        self.games_list = []
-    
+        self.result_list = []
+        self.game_list = []
+
     @property
     def username(self):
         return self._username
-
+    
     @username.setter
     def username(self, username_parameter):
-        if isinstance(username_parameter, str) and 2 <= len(username_parameter) <= 16:
+        if type(username_parameter) == str and 2 <= len(username_parameter) <= 16:
             self._username = username_parameter
 
     def results(self):
-        return self.results_list
+        return self.result_list
 
     def games_played(self):
-        return self.games_list
+        return self.game_list 
 
     def played_game(self, game):
-        if game in self.games_list:
+        if game in self.game_list:
             return True
         else: 
             return False
 
     def num_times_played(self, game):
-        result_list = [result for result in self.results_list if result.game is game]
-        return len(result_list)
+        result = [result for result in self.result_list if result.game is game]
+        return len(result)
 
 class Result:
 
     all = []
-
 
     def __init__(self, player, game, score):
         self.player = player
         self.game = game
         self.score = score
 
-        self.player.results_list.append(self)
-        
-        if not (game in self.player.games_list):
-            self.player.games_list.append(game)
+        self.player.result_list.append(self)
+
+        if not (game in self.player.game_list):
+            self.player.game_list.append(game)
 
         self.game.results_list.append(self)
 
-        if not (player in self.game.players_list):
-            self.game.players_list.append(player)
-
+        if not (player in self.game.player_list):
+            self.game.player_list.append(player)
 
         Result.all.append(self)
-
 
     @property
     def score(self):
@@ -84,7 +82,7 @@ class Result:
     
     @score.setter
     def score(self, score_parameter):
-        if (not hasattr(self, 'score')) and (type(score_parameter) == int) and 1 <= score_parameter <= 5000:
+        if (not hasattr(self, 'score')) and isinstance(score_parameter, int) and 1 <= score_parameter <= 5000:
             self._score = score_parameter
 
     @property
@@ -95,7 +93,7 @@ class Result:
     def player(self, player_parameter):
         if type(player_parameter) == Player:
             self._player = player_parameter
-
+    
     @property
     def game(self):
         return self._game
@@ -104,3 +102,4 @@ class Result:
     def game(self, game_parameter):
         if type(game_parameter) == Game:
             self._game = game_parameter
+    
